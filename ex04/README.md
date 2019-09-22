bbc6a33578ef4d753fbdd5d2395e9bcc203be703
wdEffort <yoman3011@gmail.com>
2019-09-22 22:53:54 +0900

    문자열 리터럴 UPDATE

---
+++ ex04/README.md
@@ -0,0 +1,8 @@
+# 문자열 리터럴(String literal)
+## 문자 리터럴이 1개 이상일 경우에는 문자열 리터럴(String literal)이라고 부릅니다.
+### Java에서 문자열의 표기는 ""(큰 따옴표)로 묶어서 표기합니다.
+### 문자와 달리 문자열에서는 ""(큰 따옴표)사이에 여러 개의 문자가 올 수 있습니다.
+## Package Name
+* com.ezen.example
+## Class Name
+* StringLiteralSample
---
+++ ex04/StringLiteralSample.java
@@ -0,0 +1,25 @@
+package com.ezen.example;
+
+/**
+ * 문자열 리터럴 
+ * 문자열(String)
+ * 
+ * @author yoman
+ * 
+ * 2019. 09. 22
+ * 
+ * 문자 리터럴이 1개 이상일 경우에는 문자열 리터럴(String literal)이라고 부릅니다.
+ * 
+ * Java에서 문자열의 표기는 ""(큰 따옴표)로 묶어서 표기합니다.
+ * 문자와 달리 문자열에서는 ""(큰 따옴표)사이에 여러 개의 문자가 올 수 있습니다.
+ */
+public class StringLiteralSample {
+
+	public static void main(String[] args) {
+		// 문자열 리터럴 실습 예제
+		System.out.println("Hello");
+		System.out.println("Goodbye");
+		System.out.println("안녕하세요.");
+	}
+
+}
