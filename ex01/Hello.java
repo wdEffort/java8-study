c8e9d0f11bbf65386c11bae5b90a5ba9d4c893ea
wdEffort <yoman3011@gmail.com>
2019-09-22 22:33:27 +0900

    gitignore 수정, Hello Class 수정

--- ex01/Hello.java
+++ ex01/Hello.java
@@ -5,6 +5,8 @@ package com.ezen.example;
  * 
  * @author yoman
  * 
+ * 2019. 09. 21
+ * 
  * Package는 Class를 포함하고 있는 폴더와 같은 개념입니다.
  * Class 이름을 작성할 때에는 실제 파일이름과 동일하게 작성해야 합니다.
  */
@@ -12,7 +14,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// Java 표준출력
-		System.out.println("Hello, Java!");
+		System.out.println("Hello!");
+		System.out.println("Welcome to the Java world.");
 	}
 
 }
